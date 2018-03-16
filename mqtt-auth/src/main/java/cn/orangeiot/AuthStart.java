76f308b3cb6d42d7601b24bc2fef632f428f6dc6
linuxzhangbo <zb564739784@163.com>
2018-03-16 11:02:36 +0800

    start init

--- mqtt-auth/src/main/java/cn/orangeiot/AuthStart.java
+++ mqtt-auth/src/main/java/cn/orangeiot/AuthStart.java
@@ -21,21 +21,21 @@ public class AuthStart {
     private static Logger logger = LogManager.getLogger(AuthStart.class);
 
     public static void main(String[] args) {
-//        //日志使用log4j2
-//        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.Log4j2LogDelegateFactory");
-//
-//        /**加载log4j2配置*/
-//        ConfigurationSource source = null;
-//        try {
-//            //加载log4j2配置
-//            InputStream in = AuthStart.class.getResourceAsStream("/log4j2.xml");
-//            source = new ConfigurationSource(in);
-//            Configurator.initialize(null, source);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        if (null != source) {
+        //日志使用log4j2
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.Log4j2LogDelegateFactory");
+
+        /**加载log4j2配置*/
+        ConfigurationSource source = null;
+        try {
+            //加载log4j2配置
+            InputStream in = AuthStart.class.getResourceAsStream("/log4j2.xml");
+            source = new ConfigurationSource(in);
+            Configurator.initialize(null, source);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (null != source) {
             Vertx.vertx().deployVerticle(AuthVerticle.class.getName(), rs -> {
                 if (rs.failed()) {
                     logger.error("deploy authverticle fail");
@@ -44,6 +44,6 @@ public class AuthStart {
                     logger.info("deploy authverticle successs");
                 }
             });
-//        }
+        }
     }
 }
