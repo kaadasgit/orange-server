c0a4ad6b049fa62a325f7a06ebbf30328ef89313
linuxzhangbo <zb564739784@163.com>
2018-08-08 13:28:53 +0800

    mqtt package 环境修改

--- mqtt-server/src/main/java/cn/orangeiot/mqtt/MqttServerStart.java
+++ mqtt-server/src/main/java/cn/orangeiot/mqtt/MqttServerStart.java
@@ -8,20 +8,15 @@ import cn.orangeiot.mqtt.verticle.SubscibeVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.cli.CLI;
-import io.vertx.core.cli.CLIException;
-import io.vertx.core.cli.CommandLine;
-import io.vertx.core.cli.Option;
 import io.vertx.core.json.JsonObject;
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.zookeeper.ZookeeperClusterManager;
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -39,42 +34,42 @@ public class MqttServerStart {
         start(args);
     }
 
-    static CommandLine cli(String[] args) {
-        CLI cli = CLI.create("java -jar <mqtt-broker>-fat.jar")
-                .setSummary("A vert.x MQTT Broker")
-                .addOption(new Option()
-                        .setLongName("conf")
-                        .setShortName("c")
-                        .setDescription("vert.x config file (in json format)")
-                        .setRequired(true)
-                )
-                .addOption(new Option()
-                        .setLongName("zookeeper-conf")
-                        .setShortName("hc")
-                        .setDescription("vert.x zookeeper configuration file")
-                        .setRequired(false)
-                )
-                .addOption(new Option()
-                        .setLongName("zookeeper-host")
-                        .setShortName("hh")
-                        .setDescription("vert.x zookeeper ip address of this node (es. -hh 10.0.0.1)")
-                        .setRequired(false)
-                );
-
-        // parsing
-        CommandLine commandLine = null;
-        try {
-            List<String> userCommandLineArguments = Arrays.asList(args);
-            commandLine = cli.parse(userCommandLineArguments);
-        } catch (CLIException e) {
-            // usage
-            StringBuilder builder = new StringBuilder();
-            cli.usage(builder);
-            System.out.println(builder.toString());
-//            throw e;
-        }
-        return commandLine;
-    }
+//    static CommandLine cli(String[] args) {
+//        CLI cli = CLI.create("java -jar <mqtt-broker>-fat.jar")
+//                .setSummary("A vert.x MQTT Broker")
+//                .addOption(new Option()
+//                        .setLongName("conf")
+//                        .setShortName("c")
+//                        .setDescription("vert.x config file (in json format)")
+//                        .setRequired(true)
+//                )
+//                .addOption(new Option()
+//                        .setLongName("zookeeper-conf")
+//                        .setShortName("hc")
+//                        .setDescription("vert.x zookeeper configuration file")
+//                        .setRequired(false)
+//                )
+//                .addOption(new Option()
+//                        .setLongName("zookeeper-host")
+//                        .setShortName("hh")
+//                        .setDescription("vert.x zookeeper ip address of this node (es. -hh 10.0.0.1)")
+//                        .setRequired(false)
+//                );
+//
+//        // parsing
+//        CommandLine commandLine = null;
+//        try {
+//            List<String> userCommandLineArguments = Arrays.asList(args);
+//            commandLine = cli.parse(userCommandLineArguments);
+//        } catch (CLIException e) {
+//            // usage
+//            StringBuilder builder = new StringBuilder();
+//            cli.usage(builder);
+//            System.out.println(builder.toString());
+////            throw e;
+//        }
+//        return commandLine;
+//    }
 
     public static void start(String[] args) {
         //日志使用log4j2
@@ -91,56 +86,51 @@ public class MqttServerStart {
             logger.error(e.getMessage(), e);
         }
 
+        DeploymentOptions deploymentOptions = new DeploymentOptions();
         if (null != source) {
-            CommandLine commandLine = cli(args);
-            if (commandLine == null)
-                System.exit(-1);
-
-            String confFilePath = commandLine.getOptionValue("c");
-            String zookeeperConfFilePath = commandLine.getOptionValue("hc");
-
-            DeploymentOptions deploymentOptions = new DeploymentOptions();
-            if (confFilePath != null) {
-                try {
-                    String json = FileUtils.readFileToString(new File(confFilePath), "UTF-8");
-                    JsonObject config = new JsonObject(json);
-                    deploymentOptions.setConfig(config);
-                } catch (IOException e) {
-                    logger.fatal(e.getMessage(), e);
-                }
+            JsonObject config = null;
+            try {
+                config = loadJsonConf("/config.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
             }
+            if (Objects.nonNull(config))
+                deploymentOptions.setConfig(config);
 
-            //zookeeper集群
-            if (zookeeperConfFilePath != null) {
-                try {
-                    String zkJson = FileUtils.readFileToString(new File(zookeeperConfFilePath), "UTF-8");
-                    JsonObject zkConfig = new JsonObject(zkJson);
+        }
 
-                    System.setProperty("vertx.zookeeper.hosts", zkConfig.getString("hosts.zookeeper"));
-                    ClusterManager mgr = new ZookeeperClusterManager(zkConfig);
-                    VertxOptions options = new VertxOptions().setClusterManager(mgr);
-                    if (Objects.nonNull(zkConfig.getValue("node.host")))
-                        options.setClusterHost(zkConfig.getString("node.host"));
+        //zookeeper集群
+        JsonObject zkConfig = null;
+        try {
+            zkConfig = loadJsonConf("/zkConf.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        if (Objects.nonNull(zkConfig)) {
+            System.setProperty("vertx.zookeeper.hosts", zkConfig.getString("hosts.zookeeper"));
+            ClusterManager mgr = new ZookeeperClusterManager(zkConfig);
+            VertxOptions options = new VertxOptions().setClusterManager(mgr);
+            if (Objects.nonNull(zkConfig.getValue("node.host")))
+                options.setClusterHost(zkConfig.getString("node.host"));
 
-                    Vertx.clusteredVertx(options, res -> {
-                        if (res.succeeded()) {
-                            Vertx vertx = res.result();
-                            vertx.deployVerticle(MQTTBroker.class.getName(), deploymentOptions);
-                            vertx.deployVerticle(RestApiVerticle.class.getName(), deploymentOptions);
-                            vertx.deployVerticle(PromMetricsExporter.class.getName(), deploymentOptions);
+            Vertx.clusteredVertx(options, res -> {
+                if (res.succeeded()) {
+                    Vertx vertx = res.result();
+                    vertx.deployVerticle(MQTTBroker.class.getName(), deploymentOptions);
+                    vertx.deployVerticle(RestApiVerticle.class.getName(), deploymentOptions);
+                    vertx.deployVerticle(PromMetricsExporter.class.getName(), deploymentOptions);
 
-                            vertx.deployVerticle(SubscibeVerticle.class.getName(), deploymentOptions);
-                            vertx.deployVerticle(PublishVerticle.class.getName(), deploymentOptions);
-                            vertx.deployVerticle(LogVerticle.class.getName(), deploymentOptions);
+                    vertx.deployVerticle(SubscibeVerticle.class.getName(), deploymentOptions);
+                    vertx.deployVerticle(PublishVerticle.class.getName(), deploymentOptions);
+                    vertx.deployVerticle(LogVerticle.class.getName(), deploymentOptions);
 
-                        } else {
-                            // failed!
-                            logger.fatal(res.cause().getMessage(), res.cause());
-                        }
-                    });
-                } catch (IOException e) {
-                    logger.fatal(e.getMessage(), e);
+                } else {
+                    // failed!
+                    logger.fatal(res.cause().getMessage(), res.cause());
                 }
+            });
 
 //        // use Vert.x CLI per gestire i parametri da riga di comando
 //        if(hazelcastConfFilePath!=null) {
@@ -200,23 +190,46 @@ public class MqttServerStart {
 //            } catch (FileNotFoundException e) {
 //                logger.fatal(e.getMessage(), e);
 //            }
-            } else {
-                VertxOptions options = new VertxOptions();
+        } else {
+            VertxOptions options = new VertxOptions();
 //                options.setMetricsOptions(new DropwizardMetricsOptions()
 //                        .setEnabled(true)
 //                        .setJmxEnabled(true)
 //                );
 
-                Vertx vertx = Vertx.vertx(options);
-                vertx.deployVerticle(MQTTBroker.class.getName(), deploymentOptions);
-                vertx.deployVerticle(RestApiVerticle.class.getName(), deploymentOptions);
-                vertx.deployVerticle(PromMetricsExporter.class.getName(), deploymentOptions);
+            Vertx vertx = Vertx.vertx(options);
+            vertx.deployVerticle(MQTTBroker.class.getName(), deploymentOptions);
+            vertx.deployVerticle(RestApiVerticle.class.getName(), deploymentOptions);
+            vertx.deployVerticle(PromMetricsExporter.class.getName(), deploymentOptions);
 
-                vertx.deployVerticle(SubscibeVerticle.class.getName(), deploymentOptions);
-                vertx.deployVerticle(PublishVerticle.class.getName(), deploymentOptions);
-            }
+            vertx.deployVerticle(SubscibeVerticle.class.getName(), deploymentOptions);
+            vertx.deployVerticle(PublishVerticle.class.getName(), deploymentOptions);
         }
 
+    }
+
+
+    /**
+     * @Description 加载json配置
+     * @author zhang bo
+     * @date 18-8-8
+     * @version 1.0
+     */
+    public static JsonObject loadJsonConf(String path) throws IOException {
+        InputStream in = MqttServerStart.class.getResourceAsStream(path);
+        JsonObject json = null;
+        try {
+            String config = IOUtils.toString(in, "UTF-8");//获取配置
+            if (!config.equals("")) {
+                json = new JsonObject(config);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            if (null != in)
+                in.close();
+        }
+        return json;
     }
 
 
