8c4ffd488488ab4c31881f57afa79ab4eac245f3
linuxzhangbo <zb564739784@163.com>
2018-12-27 09:01:58 +0800

    Dao field 实体

---
+++ mqtt-common/src/main/java/cn/orangeiot/common/genera/KdsUser.java
@@ -0,0 +1,16 @@
+package cn.orangeiot.common.genera;
+
+/**
+ * @author zhang bo
+ * @version 1.0
+ * @Description
+ * @date 2018-12-27
+ */
+public interface KdsUser {
+
+    //用户电话
+    String USER_TEL="userTel";
+
+    //用户邮箱
+    String USER_MAIL="userMail";
+}
